2dc0de611121fd53c66172214dcf1fc947412a3a
tauesp127-alexis <gestionti@taurus.com.mx>
2024-10-21 11:31:17 -0600

    Inicio proyecto, se agrego Bootstrap, Angular Material, Componente Home y NotFound

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink, MatButtonModule, MatSidenavModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'admin-taurus';
+  showFiller = false;
+  showCarruseles = false;
+  showSheets = false;
+  showConfiguraciones = false;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'notfound',
+        component: NotfoundComponent,
+        pathMatch: 'full'
+    },
+    // {
+    //     path: '**',
+    //     redirectTo: 'notfound',
+    //     pathMatch: 'full'
+    // }
+    
+
+];
