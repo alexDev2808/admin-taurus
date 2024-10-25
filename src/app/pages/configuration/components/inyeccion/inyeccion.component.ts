53276de6ec242b7341fd52ba7ada6fcec9ed462a
tauesp127-alexis <gestionti@taurus.com.mx>
2024-10-25 17:33:28 -0600

    cambios en pagina para configuracion de inyeccion

--- src/app/pages/configuration/components/inyeccion/inyeccion.component.ts
+++ src/app/pages/configuration/components/inyeccion/inyeccion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SliderService } from '../../../../services/slider.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inyeccion',
@@ -8,5 +10,75 @@ import { Component } from '@angular/core';
   styleUrl: './inyeccion.component.scss'
 })
 export class InyeccionComponent {
+
+  constructor(
+    private sliderService: SliderService
+  ) {}
+
+  get dataInyeccion() {
+    return this.sliderService.getDataInyeccion();
+  }
+
+  editar() {
+    Swal.fire({
+      title: "Editar:",
+      text: 'Ingresa el nombre:',
+      input: "text",
+      inputAttributes: {
+        autocapitalize: "off"
+      },
+      showCancelButton: true,
+      confirmButtonText: "Modificar",
+      showLoaderOnConfirm: true,
+      preConfirm: async (login) => {
+        try {
+          const githubUrl = `
+            https://api.github.com/users/${login}
+          `;
+          const response = await fetch(githubUrl);
+          if (!response.ok) {
+            return Swal.showValidationMessage(`
+              ${JSON.stringify(await response.json())}
+            `);
+          }
+          return response.json();
+        } catch (error) {
+          Swal.showValidationMessage(`
+            Request failed: ${error}
+          `);
+        }
+      },
+      allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: `${result.value.login}'s avatar`,
+          imageUrl: result.value.avatar_url
+        });
+      }
+    });
+  }
+
+  eliminar(index: number) {
+    Swal.fire({
+      title: "¿Confirmas la eliminación?",
+      text: `Se eliminará: ${this.dataInyeccion[index].name}`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#C62E2E",
+      cancelButtonColor: "#B7B7B7",
+      cancelButtonText: "Cancelar",
+      confirmButtonText: "Si, eliminar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Eliminado!",
+          text: "El elemento ha sido eliminado!",
+          icon: "success"
+        });
+      }
+    });
+  }
+
 
 }
